56debbe51be8af937ce73699143fff176f912982
Abdur Rehman <wabdulrehman@yahoo.com>
2021-03-31 23:21:18 +0100

    add a feature to wrap bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,17 @@
 require_relative "bike"
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :station
+  def initialize
+    @station = []
+  end  
   def release_bike
-    fail 'there are no bikes' unless @bike
-    @bike
+    fail 'there are no bikes' if station.empty?
+    station.pop
   end
-
   def docking(bike)
-    fail 'station is full' if @bike
-    @bike = bike
+    fail 'station is full' if @station.length >= 20
+    @station << bike
+    bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,9 @@ require "docking_station"
 describe DockingStation do
   describe '#release_bike' do
     it { is_expected.to respond_to :release_bike }
-    it "releases working bikes" do
-      bike = Bike.new
-      expect(bike).to be_working
+    it "releases a working bikes" do
+      subject.docking(Bike.new)
+      expect(subject.release_bike).to be_working
     end
     it 'return an error if there are no bikes' do
       expect { subject.release_bike }.to raise_error 'there are no bikes'
@@ -18,7 +18,7 @@ describe DockingStation do
       expect(subject.docking(bike)).to eq(bike)
     end
     it 'return an error if docking station is full' do
-      subject.docking(Bike.new)
+      20.times { subject.docking(Bike.new) }
       expect { subject.docking(Bike.new) }.to raise_error 'station is full'
     end
   end 
